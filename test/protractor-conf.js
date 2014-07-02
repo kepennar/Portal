242271f42977860d83665dd8bd1f1765e39d5d4b
Kevin Pennarun <kevin.pennarun@gmail.com>
2014-07-02 09:39:15 +0200

    Protractor conf

--- test/protractor-conf.js
+++ test/protractor-conf.js
@@ -1,24 +1,26 @@
 exports.config = {
-		
-	config.sauceUser = process.env.SAUCE_USERNAME;
-	config.sauceKey = process.env.SAUCE_ACCESS_KEY;
-	config.capabilities = {
-		'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
-		'build': process.env.TRAVIS_BUILD_NUMBER
+	
+	// Run e2e tests on sauce lab when project is build on Travis
+	if (process.env.TRAVIS) {	
+		sauceUser = process.env.SAUCE_USERNAME;
+		sauceKey = process.env.SAUCE_ACCESS_KEY;
+		capabilities = {
+			'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+			'build': process.env.TRAVIS_BUILD_NUMBER
+		}
 	}
 	allScriptsTimeout: 11000,
 	baseUrl: 'http://localhost:9001/app/',
-	capabilities: {
-		'browserName': 'firefox'
-	},
+	capabilities.browserName: 'firefox'
+},
 
-	specs: [
-    	'e2e/*.js'
-  	],
+specs: [
+'e2e/*.js'
+],
 
-  	framework: 'jasmine',
-	jasmineNodeOpts: {
-		showColors: true,
-		defaultTimeoutInterval: 30000
-	}
+framework: 'jasmine',
+jasmineNodeOpts: {
+	showColors: true,
+	defaultTimeoutInterval: 30000
+}
 };
