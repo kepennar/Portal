8e62a252540eb83c42d5c06bd59521f734ca6442
Kevin Pennarun <kevin.pennarun@gmail.com>
2014-07-02 08:59:42 +0200

    Add Sauce-lab config (Sauce Lab=>Testing cloud service)

--- test/protractor-conf.js
+++ test/protractor-conf.js
@@ -1,5 +1,11 @@
 exports.config = {
 		
+	config.sauceUser = process.env.SAUCE_USERNAME;
+	config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+	config.capabilities = {
+		'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+		'build': process.env.TRAVIS_BUILD_NUMBER
+	}
 	allScriptsTimeout: 11000,
 	baseUrl: 'http://localhost:9001/app/',
 	capabilities: {
