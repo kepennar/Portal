b518d3d8d935b3fef31278b54fcad38625cd338f
Kevin Pennarun <kevin.pennarun@gmail.com>
2014-07-06 12:05:53 +0200

    E2e tests on Sauce Lab

--- test/protractor-conf.js
+++ test/protractor-conf.js
@@ -1,17 +1,20 @@
+// Run e2e tests on sauce lab when project is build on Travis
+
+var capabilities = {};
+capabilities.browserName: 'chrome';
+if (process.env.TRAVIS) {	
+	sauceUser = process.env.SAUCE_USERNAME;
+	sauceKey = process.env.SAUCE_ACCESS_KEY;
+	capabilities.tunnel-identifier= process.env.TRAVIS_JOB_NUMBER;
+	capabilities.build = process.env.TRAVIS_BUILD_NUMBER;
+}
+
 exports.config = {
 	
-	// Run e2e tests on sauce lab when project is build on Travis
-	if (process.env.TRAVIS) {	
-		sauceUser = process.env.SAUCE_USERNAME;
-		sauceKey = process.env.SAUCE_ACCESS_KEY;
-		capabilities = {
-			'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
-			'build': process.env.TRAVIS_BUILD_NUMBER
-		}
-	}
+	capabilities: capabilities,
+	
 	allScriptsTimeout: 11000,
-	baseUrl: 'http://localhost:9001/app/',
-	capabilities.browserName: 'firefox'
+	baseUrl: 'http://localhost:9001/app/'
 },
 
 specs: [
